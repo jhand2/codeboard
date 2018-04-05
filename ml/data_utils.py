a10bcdd54ec883c1b82cd62f5eaaa895e39d3561
Jordan Hand <jordanhand22@gmail.com>
2018-04-04 21:25:41 -0700

    finish data loading, add neural network

--- ml/data_utils.py
+++ ml/data_utils.py
@@ -1,8 +1,76 @@
 import os
 import imageio
 import numpy as np
+import h5py
+import tarfile
 
-def load_dir(dirname, image_size, maxn=None):
+image_size = 45 # size for this data doesn't change
+
+def divide_data(dataset_X, dataset_Y):
+    """
+    Divide `dataset` into 3 datasets, train, test, and validation
+    """
+    n = dataset_X.shape[0]
+    cut1 = round(n * 0.6)
+    cut2 = round(n * 0.8)
+
+    p = np.random.permutation(n)
+    shuffle_X = dataset_X[p]
+    shuffle_Y = dataset_Y[p]
+    
+    train_X = shuffle_X[:cut1]
+    train_Y = shuffle_Y[:cut1]
+
+    test_X = shuffle_X[cut1:cut2]
+    test_Y = shuffle_Y[cut1:cut2]
+
+    valid_X = shuffle_X[cut2:]
+    valid_Y = shuffle_Y[cut2:]
+
+    return {
+        "train_X": train_X,
+        "train_Y": train_Y,
+        "test_X": test_X,
+        "test_Y": test_Y,
+        "valid_X": valid_X,
+        "valid_Y": valid_Y
+    }
+
+def load_all_data():
+    """
+    Load each datapoint from the math symbols dataset and cache the formatted
+    arrays in an h5 file. If the data is already cached, simply load from
+    the h5 file.
+
+    returns
+        A dictionary containing
+        - dataset_X: 3D numpy array of images
+        - dataset_Y: 2D numpy array of integer labels corresponding to dataset_X.
+                     The integer label corresponds to an index in `labels`
+        - labels: Names of labels
+    """
+    full_path = os.path.join(os.path.dirname(__file__), "data.h5")
+    if os.path.isfile(full_path):
+        print("Loading dataset from file")
+        # read h5 file
+        h5f = h5py.File("data.h5", "r")
+        return {
+            "dataset_X": h5f["dataset_X"].value,
+            "dataset_Y": h5f["dataset_Y"].value,
+            "labels": h5f["labels"].value
+        }
+    else:
+        print("Loading dataset and saving as data.h5")
+        return package_dataset()
+
+def load_dir(dirname, maxn=None):
+    """
+    Loads all the images from the directory for symbol `dirname` into
+    a 3D numpy array
+
+    returns
+        A 3D numpy array containing all images for symbol `dirname`
+    """
     os.path.split(__file__)
     full_path = os.path.join(os.path.dirname(__file__), "data", dirname)
     files = os.listdir(full_path)
@@ -12,30 +80,33 @@ def load_dir(dirname, image_size, maxn=None):
     else:
         ndata = min(maxn, len(files))
 
-    dataset = np.ndarray(shape=(ndata, image_size, image_size),
+    dataset = np.ndarray(shape=(ndata, image_size ** 2),
                          dtype=np.float32)
 
     nimages = 0
-    for i in range(len(files)):
-        if i >= ndata:
-            break
-
+    for i in range(ndata):
         impath = os.path.join(full_path, files[i])
         idata = imageio.imread(impath)
 
         if idata.shape != (image_size, image_size):
             raise Exception('Unexpected image shape: %s' % str(idata.shape))
 
-        dataset[i, :, :] = idata
+        dataset[i, :] = idata.reshape((image_size ** 2, ))
         nimages += 1
-
-    dataset = dataset[0:nimages, :, :] # only return as many images as we loaded
 
     return dataset
 
-def load_all_data(image_size):
+def package_dataset():
+    """
+    Loads each symbol in the dataset and saves the whole set as numpy
+    arrays in an h5 file. The arrays are as follows
+        - dataset_X: 3D numpy array of images
+        - dataset_Y: 2D numpy array of integer labels corresponding to dataset_X.
+                     The integer label corresponds to an index in `labels`
+        - labels: Names of labels
+    """
     data_path = os.path.join(os.path.dirname(__file__), "data")
-    dirnames = os.listdir(data_path)
+    dirnames = sorted(os.listdir(data_path))
 
     # Get total number of images
     nimg = 0
@@ -44,20 +115,35 @@ def load_all_data(image_size):
         nimg += len(imgs)
 
     print("Images in dataset:", nimg)
-    train_X = np.zeros((nimg, image_size, image_size))
-    train_Y = np.zeros((nimg, 1))
+    dataset_X = np.zeros((nimg, image_size ** 2))
+    dataset_Y = np.zeros((nimg, len(dirnames)))
 
-    # TODO: Split dataset into train, test, valid. Return all 3
+    offset = 0
+    maxn = 1000 # max images to use per letter
+    for i in range(len(dirnames)):
+        data = load_dir(dirnames[i], maxn)
+        dataset_X[offset:offset+data.shape[0], :] = data
+        dataset_Y[offset:offset+data.shape[0], i] = 1
+        print("%s loaded" % dirnames[i])
+        offset += data.shape[0]
 
-    # Sample code for shuffling. will use later
-    # p = numpy.random.permutation(len(a))
+    print("Images used:", offset)
+
+    dataset_X = dataset_X[:offset]
+    dataset_Y = dataset_Y[:offset]
 
     ret = {
-        "train_X": train_X,
-        "train_Y": train_Y,
+        "dataset_X": dataset_X,
+        "dataset_Y": dataset_Y,
         "labels": dirnames
     }
 
-    return ret
+    h5f = h5py.File("data.h5", "w")
+    h5f.create_dataset("dataset_X", data=dataset_X)
+    h5f.create_dataset("dataset_Y", data=dataset_Y)
+    h5f.create_dataset("labels", data=np.asarray(dirnames, dtype=np.dtype("S")))
 
+    h5f.close()
+
+    return ret
 
--- ml/network.py
+++ ml/network.py
@@ -1,6 +1,153 @@
 #!/usr/bin/python3
-from data_utils import load_all_data
+import warnings
+with warnings.catch_warnings():
+    warnings.filterwarnings("ignore",category=FutureWarning)
+    import tensorflow as tf
+    from data_utils import *
+
+
+class Network:
+    def __init__(self, labels):
+        # TODO: Regularization: dropout
+
+        self.labels = labels
+        self.batch_size = 256
+        self.image_size = 45 ** 2
+        self.layers = [
+            (128, "relu"),
+            (256, "relu"),
+            (256, "relu"),
+            (256, "relu"),
+            (128, "relu"),
+            (len(labels), None)
+        ]
+
+        # set up neural net
+        self.graph = tf.Graph()
+        with self.graph.as_default():
+            self.global_step = tf.Variable(0)
+            self.learning_rate = tf.train.exponential_decay(0.0001, self.global_step, self.batch_size, 0.95) 
+            self.weights = {}
+            self.biases = {}
+
+            self.tf_train_dataset = tf.placeholder(tf.float32,
+                                                   shape=(self.batch_size, self.image_size),
+                                                   name="tf_train_dataset")
+            self.tf_train_labels = tf.placeholder(tf.float32,
+                                                  shape=(self.batch_size, len(self.labels)),
+                                                  name="tf_train_labels")
+
+            # Initialize weights and biases
+            insize = self.image_size
+            for i in range(len(self.layers)):
+                outsize = self.layers[i][0]
+                self.weights[i] = tf.Variable(tf.truncated_normal([insize, outsize], stddev=(2.0/outsize)))
+                self.biases[i] = tf.Variable(tf.zeros([outsize]))
+                insize = outsize
+
+            self.session = tf.Session(graph=self.graph)
+
+    def forward_prop(self, X):
+        inlayer = X
+        for i in range(len(self.layers)):
+            layer = tf.add(tf.matmul(inlayer, self.weights[i]), self.biases[i])
+            fname = self.layers[i][1]
+            if fname == "relu":
+                layer = tf.nn.relu(layer)
+            elif fname == "softmax":
+                layer = tf.nn.softmax(layer)
+            inlayer = layer
+
+        return inlayer
+    
+        
+    def accuracy(self, predictions, labels):
+        return (100.0 * np.sum(np.argmax(predictions, 1) == np.argmax(labels, 1))
+              / predictions.shape[0])
+        
+
+    def train(self, X, Y, valid_X=None, valid_Y=None, test_X=None, test_Y=None):
+        with self.graph.as_default():
+            logits = self.forward_prop(self.tf_train_dataset)
+
+            nonreg_loss = tf.reduce_mean(
+                        tf.nn.softmax_cross_entropy_with_logits_v2(labels=self.tf_train_labels, logits=logits))
+            l2_regularization_penalty = 0.01
+            l2_loss = 0
+            for i in range(len(self.layers)):
+                l2_loss += (l2_regularization_penalty * tf.nn.l2_loss(self.weights[i]))
+
+            loss = nonreg_loss + l2_loss
+
+            optimizer = tf.train.AdamOptimizer(self.learning_rate).minimize(loss)
+            train_prediction = tf.nn.softmax(logits)
+
+            if valid_X is not None:
+                tf_valid_dataset = tf.constant(valid_X)
+                valid_prediction = tf.nn.softmax(self.forward_prop(tf_valid_dataset))
+
+            if test_X is not None:
+                tf_test_dataset = tf.constant(test_X)
+                test_prediction = tf.nn.softmax(self.forward_prop(tf_test_dataset))
+            self.session.run(tf.global_variables_initializer())
+
+        num_steps = 8001
+
+        with self.session.as_default():
+            for step in range(num_steps):
+                # Pick an offset within the training data, which has been randomized.
+                # Note: we could use better randomization across epochs.
+                offset = (step * self.batch_size) % (Y.shape[0] - self.batch_size)
+
+                # Generate a minibatch.
+                batch_data = X[offset:(offset + self.batch_size), :]
+                batch_labels = Y[offset:(offset + self.batch_size), :]
+
+                # Prepare a dictionary telling the session where to feed the minibatch.
+                # The key of the dictionary is the placeholder node of the graph to be fed,
+                # and the value is the numpy array to feed to it.
+                feed_dict = {"tf_train_dataset:0": batch_data, "tf_train_labels:0": batch_labels}
+
+                _, l, predictions = self.session.run([optimizer, loss, train_prediction], feed_dict=feed_dict)
+
+                if (step % 500 == 0):
+                    print("Minibatch loss at step %d: %f" % (step, l))
+                    print("Minibatch accuracy: %.1f%%" % self.accuracy(predictions, batch_labels))
+                    if valid_X is not None:
+                        print("Validation accuracy: %.1f%%" % self.accuracy(
+                              valid_prediction.eval(), valid_Y))
+            if test_X is not None:
+                print("Test accuracy: %.1f%%" % self.accuracy(test_prediction.eval(), test_Y))
+
+    def predict(self, X):
+        pass
 
 if __name__ == "__main__":
-    data_cache = load_all_data(45)
-    print(data_cache["labels"])
+    data_cache = load_all_data()
+    dataset_X = data_cache["dataset_X"]
+    dataset_Y = data_cache["dataset_Y"]
+    print(dataset_Y.shape)
+    labels = data_cache["labels"]
+
+    # Shuffle data and divide it into train, test, and validate
+    sets = divide_data(dataset_X, dataset_Y)
+
+    train_X = sets["train_X"].astype(np.float32)
+    train_Y = sets["train_Y"].astype(np.float32)
+
+    test_X = sets["test_X"].astype(np.float32)
+    test_Y = sets["test_Y"].astype(np.float32)
+
+    valid_X = sets["valid_X"].astype(np.float32)
+    valid_Y = sets["valid_Y"].astype(np.float32)
+
+    print("Training set shape:", train_X.shape)
+    print("Training labels shape:", train_Y.shape)
+
+    print("Test set shape:", test_X.shape)
+    print("Validation set shape:", valid_X.shape)
+
+    nn = Network(labels)
+    nn.train(train_X, train_Y, valid_X, valid_Y, test_X, test_Y)
+    nn.session.close()
+
